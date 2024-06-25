6d07db121744addbc7433e331cef51a1dd5925fe
Zohaib Yasin <zohaiby33@gmail.com>
2024-06-25 17:17:20 +0500

    Done with java-script basics

--- first-app.js
+++ first-app.js
@@ -1,3 +1,18 @@
 const fs = require("fs");
 
-fs.writeFileSync("hello.txt", "Hello from Node.js! 🚀 by zohiab");
+fs.writeFileSync("hello.txt", "Hello from Node.js! 🚀 by zohiab Yasin");
+
+var array = [1, 2, 3, 4, 5, 6, 7, "T", 8, 9, 10];
+var copiedArray = array.slice(0, "T");
+console.log(copiedArray);
+
+// rest operator is used to merg elements in an array
+const toArray = (...args) => {
+  return args;
+};
+console.log(toArray(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+// spread operator is used to split elements in an array
+let array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let array2 = [...array1];
+console.log(array2);
